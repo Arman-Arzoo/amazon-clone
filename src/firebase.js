d08aa376694b9c229fd9ffab3f3eb8197a52a379
Arman Ali <toplinegeeks@gmail.com>
2021-11-21 16:52:45 +0500

    basic react app setup with firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLalbMR02tEJLZxSQmc1p3XNajDWgJnOQ",
+  authDomain: "clone-7d451.firebaseapp.com",
+  projectId: "clone-7d451",
+  storageBucket: "clone-7d451.appspot.com",
+  messagingSenderId: "289825310307",
+  appId: "1:289825310307:web:16dc5c2d2e7250b1de5a56",
+  measurementId: "G-YYHLY05L4V",
+};

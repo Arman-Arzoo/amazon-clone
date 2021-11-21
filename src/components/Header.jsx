5ee49961ec303acd101401fd22659a2740aaa2a9
Arman Ali <toplinegeeks@gmail.com>
2021-11-21 18:05:14 +0500

    header logo and header container deisgn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ import "./header.css";
 export const Header = () => {
   return (
     <div className="header">
-      <img src="" alt="" className="header__logo" />
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiY_ABgnclSsHClv2v1T-CJ3taKt16phhwe-FzXWg6qM4A5oEK-xHmNr7RH8KRPeZk7A&usqp=CAU"
+        alt=""
+        className="header__logo"
+      />
 
       <div className="header__search">
         <input type="text" className="header__searchInput" />
@@ -15,13 +19,13 @@ export const Header = () => {
         </div>
 
         <div className="header__navOption">
-          <span className="header__navOptionOne">Hello Arman</span>
-          <span className="header__navOptionTwo">Sign in</span>
+          <span className="header__navOptionOne">Retrun</span>
+          <span className="header__navOptionTwo">& Orders</span>
         </div>
 
         <div className="header__navOption">
-          <span className="header__navOptionOne">Hello Arman</span>
-          <span className="header__navOptionTwo">Sign in</span>
+          <span className="header__navOptionOne">Your</span>
+          <span className="header__navOptionTwo">Prime</span>
         </div>
       </div>
     </div>

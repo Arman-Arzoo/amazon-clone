3580d4b7136f61e53d60ed435512fe38e1bc67b2
Arman Ali <toplinegeeks@gmail.com>
2021-11-22 10:18:07 +0500

    update header.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Header } from "./components/Header.jsx";
+import { Home } from "./components/Home";
 function App() {
   return (
     <div className="app">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__container"></div>
+    </div>
+  );
+};

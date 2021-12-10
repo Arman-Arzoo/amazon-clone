eee981a7ae0bbdd155b0a31113ee2de8fe031dd9
Arman Ali <toplinegeeks@gmail.com>
2021-12-10 19:23:39 +0500

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,15 @@
 export const initialState = {
   basket: [],
 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_BUSKET":
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+  }
+};
+
+export default reducer;

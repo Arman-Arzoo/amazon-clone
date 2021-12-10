5bad2bdec6792a9aec08611693f7647144a575f9
Arman Ali <toplinegeeks@gmail.com>
2021-12-10 19:24:37 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import reducer, { initialState } from "./reducer";
+import { StateProvider } from "./StateProvider";
 
 ReactDOM.render(
   <React.StrictMode>
